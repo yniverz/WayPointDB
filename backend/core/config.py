ff5686f0f3bbbdd60ac6a6659a0562ad76b907a5
yniverz <88168412+yniverz@users.noreply.github.com>
2025-01-31 00:22:08 +0100

    add DB_PORT configuration and update docker-compose port mapping

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,12 +4,13 @@ import uuid
 class Config:
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "test"  # For local dev
     DB_HOST = os.environ.get("POSTGRES_HOST")
+    DB_PORT = os.environ.get("POSTGRES_PORT", 5432)
     DB_NAME = os.environ.get("POSTGRES_DB")
     DB_USER = os.environ.get("POSTGRES_USER")
     DB_PASS = os.environ.get("POSTGRES_PASSWORD", "password")
     UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "/app/imports")
     BACKGROUND_MAX_THREADS = os.getenv("BACKGROUND_MAX_THREADS", 1)
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PHOTON_SERVER_HOST = os.getenv("PHOTON_SERVER_HOST", "")
     PHOTON_SERVER_HTTPS = os.getenv("PHOTON_SERVER_HTTPS", True)
--- backend/core/extensions.py
+++ backend/core/extensions.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restx import Api
 from flask import Flask
+from sqlalchemy import create_engine
 
 db = SQLAlchemy()
 migrate = Migrate()
