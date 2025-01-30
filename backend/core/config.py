e1a383ce1ffa51986cbf24cf1909276995a25ce6
yniverz <88168412+yniverz@users.noreply.github.com>
2025-01-30 21:52:20 +0100

    fix postgres access issues

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,11 +3,11 @@ import uuid
 
 class Config:
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "test"  # For local dev
-    DB_HOST = os.getenv("POSTGRES_HOST", "db")
-    DB_NAME = os.getenv("POSTGRES_DB", "mydatabase")
-    DB_USER = os.getenv("POSTGRES_USER", "user")
-    DB_PASS = os.getenv("POSTGRES_PASSWORD", "password")
-    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "/app/uploads")
+    DB_HOST = os.environ.get("POSTGRES_HOST")
+    DB_NAME = os.environ.get("POSTGRES_DB")
+    DB_USER = os.environ.get("POSTGRES_USER")
+    DB_PASS = os.environ.get("POSTGRES_PASSWORD", "password")
+    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "/app/imports")
     BACKGROUND_MAX_THREADS = os.getenv("BACKGROUND_MAX_THREADS", 1)
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
