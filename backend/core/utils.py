daa5e7aaa2db6452d5e2322fcbcb67dd2e39dcbd
yniverz <88168412+yniverz@users.noreply.github.com>
2025-02-13 17:26:47 +0100

    refactor: comment out trace_id column addition in import table

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -107,7 +107,7 @@ def create_default_user():
     # cursor.execute("ALTER TABLE daily_statistic ADD COLUMN trace_id UUID")
     # cursor.execute("ALTER TABLE additional_trace ADD COLUMN owner_id UUID NOT NULL")
     # cursor.execute("ALTER TABLE additional_trace RENAME COLUMN user_id_list TO share_with_list")
-    cursor.execute("ALTER TABLE import ADD COLUMN trace_id UUID")
+    # cursor.execute("ALTER TABLE import ADD COLUMN trace_id UUID")
 
 
 
